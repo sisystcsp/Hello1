2ccb3915f3498da173f16aec0c015e6ef9074bb4
sisystcsp <s.inaba.systcsp@gmail.com>
2024-12-17 19:26:56 +0900

    Update main.py
    getcwd test

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     return {"Hello": "World"}
+
+@app.get("/pwd")
+def pwd():
+    path = os.getcwd()
+    print(f'path={path}')
+    return path
